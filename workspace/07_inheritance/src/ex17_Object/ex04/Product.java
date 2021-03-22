6f4c5f5aac7ea809e3adb3dd90569804d4de5c2d
koreaitlecture <min-k-t@hanmail.net>
2021-03-22 14:04:29 +0900

    update 2021-03-22

---
+++ workspace/07_inheritance/src/ex17_Object/ex04/Product.java
@@ -0,0 +1,17 @@
+package ex17_Object.ex04;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+	// field
+	private String model;  // 모델
+	private String manufacturer;  // 제조사
+	private String date;  // 제조일자
+	
+}
