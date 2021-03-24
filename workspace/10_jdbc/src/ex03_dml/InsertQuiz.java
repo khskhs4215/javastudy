6255c2566f72b19229cca7bf4cee5e9c5af5c56e
koreaitlecture <min-k-t@hanmail.net>
2021-03-24 14:55:21 +0900

    update 2021-03-24

--- workspace/10_jdbc/src/ex03_dml/InsertQuiz.java
+++ workspace/10_jdbc/src/ex03_dml/InsertQuiz.java
@@ -1,11 +1,34 @@
 package ex03_dml;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class InsertQuiz {
 
 	public static void main(String[] args) {
 		
 		// 임의의 3명의 staff을 삽입하세요.
 		// 반복문과 배열을 사용하세요.
+		int[] noList = { 3, 4, 5 };
+		String[] nameList = { "david", "james", "jack" };
+		String[] departList = { "총무", "영업", "고객" };
+		String[] hireDateList = { "SYSDATE", "21/03/15", "21/03/20" };
+		
+		Connection con = null;
+		PreparedStatement ps = null;
+		
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ps != null) { ps.close(); }
+				if (con != null) { con.close(); }
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 
 	}
 
