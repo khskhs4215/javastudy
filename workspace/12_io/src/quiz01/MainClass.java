adbe72493b048d5d3331fbef7638e9c681eb341b
koreaitlecture <min-k-t@hanmail.net>
2021-03-26 15:50:38 +0900

    update 2021-03-26

---
+++ workspace/12_io/src/quiz01/MainClass.java
@@ -0,0 +1,44 @@
+package quiz01;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
+// 사용자로부터 국, 영, 수 점수를 입력 받은 뒤,
+// int[] 배열에 저장한다.
+// score.dat 파일에 이름, 입력 받은 점수, 평균을 저장하시오.
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String[] subjects = {"국어", "영어", "수학", "과학"};
+		int[] scores = new int[subjects.length];
+		String name = "james";
+		int total = 0;
+		double ave = 0;
+		
+		for (int i = 0; i < subjects.length; i++) {
+			System.out.print(subjects[i] + " >>> ");
+			scores[i] = sc.nextInt();
+			total += scores[i];
+		}
+		ave = total / subjects.length;
+		
+		try (DataOutputStream dos = new DataOutputStream(new FileOutputStream("score.dat"))) {
+			dos.writeInt(subjects.length);
+			dos.writeUTF(name);
+			for (int score : scores) {
+				dos.writeInt(score);
+			}
+			dos.writeDouble(ave);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		sc.close();
+		
+	}
+
+}
