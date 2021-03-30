496891b832401af7ddfe142c35601361a586d8c3
koreaitlecture <min-k-t@hanmail.net>
2021-03-30 10:17:39 +0900

    update 2021-03-30

--- workspace/13_thread/src/ex03_join/Calculator.java
+++ workspace/13_thread/src/ex03_join/Calculator.java
@@ -1,12 +1,18 @@
 package ex03_join;
 
-public class Calculator {
+public class Calculator extends Thread {
 
 	// field
+	private int from;
+	private int to;
 	private int result;
 	
 	// method
-	public void addition(int from, int to) {
+	@Override
+	public void run() {
+		addition();
+	}
+	public void addition() {
 		// from부터 to까지 모든 정수 더해서 result에 저장
 		if (from > to) {
 			int temp = from;
@@ -22,6 +28,18 @@ public class Calculator {
 	}
 	public void setResult(int result) {
 		this.result = result;
+	}
+	public int getFrom() {
+		return from;
+	}
+	public void setFrom(int from) {
+		this.from = from;
+	}
+	public int getTo() {
+		return to;
+	}
+	public void setTo(int to) {
+		this.to = to;
 	}
 	
 }
--- workspace/13_thread/src/ex03_join/MainClass.java
+++ workspace/13_thread/src/ex03_join/MainClass.java
@@ -6,8 +6,25 @@ public class MainClass {
 		
 		Calculator calc1 = new Calculator();
 		Calculator calc2 = new Calculator();
-		calc1.addition(1, 500);
-		calc2.addition(501, 1000);
+		
+		calc1.setFrom(1);
+		calc1.setTo(500);
+		calc2.setFrom(501);
+		calc2.setTo(1000);
+		
+		calc1.start();
+		calc2.start();
+		
+		// main이 calc1 스레드와 calc2 스레드가 종료될때까지 기다려야 합니다.
+		// 스레드 종료를 기다리는 메소드 : join()
+		// join() 호출은 예외처리가 필요합니다.
+		try {
+			calc1.join();
+			calc2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 		System.out.println(calc1.getResult() + calc2.getResult());
 		
 	}
